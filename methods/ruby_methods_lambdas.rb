602d7ba0060e9af263b796c0ca74af8e74f2c100
Miguel Rentes <miguel.rentes@gmail.com>
2017-04-17 00:00:30 +0100

    Adds solution for the Ruby Methods - Lambdas challenge.

--- methods/ruby_methods_lambdas.rb
+++ methods/ruby_methods_lambdas.rb
@@ -1,20 +1,20 @@
 # Write a lambda which takes an integer and square it
-square      = #_____________________________________
+square      = ->(a) {a**2}
 
 # Write a lambda which takes an integer and increment it by 1
-    plus_one    = #_____________________________________
+plus_one    = ->(a) {a+1}
 
 # Write a lambda which takes an integer and multiply it by 2
-        into_2      = #_____________________________________
+into_2      = ->(a) {a * 2}
 
 # Write a lambda which takes two integers and adds them
-            adder       = #_____________________________________
+adder       = ->(a, b) {a + b}
 
 # Write a lambda which takes a hash and returns an array of hash values
-                values_only = #_____________________________________
+values_only = ->(hash) {hash.values}
 
 
-                    input_number_1 = gets.to_i
+input_number_1 = gets.to_i
 input_number_2 = gets.to_i
 input_hash = eval(gets)
 
