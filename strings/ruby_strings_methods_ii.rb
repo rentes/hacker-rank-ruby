13d647a5e5acf522e58371401a4ec3b52bbdd668
Miguel Rentes <miguel.rentes@gmail.com>
2017-04-18 23:30:50 +0100

    Adds solution for the Ruby Strings - Methods II challenge.

--- strings/ruby_strings_methods_ii.rb
+++ strings/ruby_strings_methods_ii.rb
@@ -1,14 +1,12 @@
 def strike(string)
-  new_string = string.prepend("<strike>")
-  new_string = new_string.concat("</strike>")
+  string.prepend("<strike>").concat("</strike>")
 end
 
 def mask_article(string, array)
-  new_string = ''
   array.each do |s|
     if string.include?(s)
-      new_string = string.gsub(/#{s}/, strike(s))
+      string.gsub!("#{s}", strike(s))
     end
   end
-  new_string
+  string
 end
